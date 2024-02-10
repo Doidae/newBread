b7345fa628d7dede5b8201d0a2c9b3436b89df37
Doidae <andyarredondo1224@gmail.com>
2024-02-10 17:42:35 -0500

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -21,9 +21,11 @@ function Show ({bread, index}) {
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE"/>
             </form>
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
         </Default>
     )
 }
 
 
 module.exports = Show
+//TODO: Add favicon
