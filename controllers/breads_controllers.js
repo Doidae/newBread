2ecc1c326db9a65a1cf8ace3be1d0bd2ea5ef6b4
Doidae <andyarredondo1224@gmail.com>
2024-02-10 19:08:21 -0500

    Added drop down and baked by message

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -60,14 +60,14 @@ breads.get('/:id/edit', (req, res) => {
 breads.get('/:id', (req, res) => {
     Bread.findById(req.params.id)
         .then(foundBread => {
+            const bakedBy = foundBread.getBakedBy() 
+            console.log(bakedBy)
             res.render('show', {
                 bread: foundBread
             })
         })
-        .catch(err => {
-            res.send('404')
-        })
     })
+
 
 
 
